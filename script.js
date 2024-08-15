411f0b46458894981d4a71dcae6f2a26dc74c93b
Nhlalo <nhlalonkosi@gmail.com>
2024-08-15 19:21:23 +0200

    Change the name of the function to getComputerChoi ce
    This will correctly describe what the function does.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const paper = 1; // Set the variable of paper to 1
 const scissors = 2; // Set the variable of scissors to 2
 
 //This is a function to make the computer randomly choose between 0,1 and 2.
-function getComputerChoiceO(cmputerChoice){
+function getComputerChoice(cmputerChoice){
     return (Math.floor(Math.random() * 3)) // Expected outputL 0, 1 or 2
 }
 
