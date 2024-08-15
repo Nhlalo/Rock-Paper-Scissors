a5a41977b8d75689558e306d84e3c5f70964249f
Nhlalo <nhlalonkosi@gmail.com>
2024-08-15 19:14:54 +0200

    Change the wording of the comment
    This reactifies the mistake made when exlpaining the function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(Math.floor(Math.random() * 3))
+const rock = 0; // Set the variable of rock to 0
+const paper = 1; // Set the variable of paper to 1
+const scissors = 2; // Set the variable of scissors to 2
+
+//This is a function to make the computer randomly choose between 0,1 and 2.
+function getComputerChoiceO(cmputerChoice){
+    return (Math.floor(Math.random() * 3)) // Expected outputL 0, 1 or 2
+}
