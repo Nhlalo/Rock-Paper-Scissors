49f30bf39d0c62870ed099dc7c08db109592ea33
Nhlalo <nhlalonkosi@gmail.com>
2024-08-18 23:57:48 +0200

    Place the two variables within the loop
    This is to ensure that variable are called every the loop runs rather than once.

--- script.js
+++ script.js
@@ -33,17 +33,20 @@ if(humanChoice == 'rock' && computerChoice == 2){
 }else if(humanChoice == 'scissor' && computerChoice == 0){
     console.log( 'You lose! Rock beats Scissor');
     ++computerScore;
+}else{
+    console.log("It's a draw!")
 }
 
 }
 
 //This function will determine the winner of the game of 5 rounds of play
 function playGame(){
-    const humanSelection = getHumanChoice(); // make the variable equal to the getHumanChoice function
-    const computerSelection = getComputerChoice(); // make the variable equal to the getComputerChoice function
     
     //This will make sure there are five rounds to play in order to determine the overall winner and if the winner has been decided, the game will reset
     for( let i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice(); // make the variable equal to the getHumanChoice function
+        const computerSelection = getComputerChoice(); // make the variable equal to the getComputerChoice function
+    
         playRound(humanSelection, computerSelection);
         //If the user reaches the score of 5 first, the code within will be executed
         if(humanScore == 5){
@@ -51,13 +54,13 @@ function playGame(){
             humanScore = 0; // Set the human score to 0;
             computerScore = 0; // Set the human score to 0;
             playRound(humanSelection, computerSelection);
-        }else{ //If the user reaches the score of 5 first, the code within will be executed
+        }else if(computerScore == 5){ //If the user reaches the score of 5 first, the code within will be executed
             console.log('You lose the 5 game series!')
             humanScore = 0;
             computerScore = 0;
             playRound(humanSelection, computerSelection);
         }
-        i++
+        
     }
 }
 playGame(); // This will call the play game function./
